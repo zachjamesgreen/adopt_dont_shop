4eb59ebf65b4ec478fb16e566b2fa50b73898cb9
Zach Green <zachjamesgreen@gmail.com>
2021-05-20 11:11:52 -0600

    add enums to model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
   belongs_to :pets
   # belongs_to :users
-  enum status: %w[draft reviewed published]
+  enum status: %w(in_progress pending accepted rejected)
 end
